af52247c4bd8d6bc089c009915fc64ee8ab14433
nanidangeti <nanid9611@gmail.com>
2024-02-06 12:08:24 +0530

    Add project files.

---
+++ WebApplicationfilters/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace WebApplicationfilters.Controllers
+{
+    public class EmployeeController : Controller,IActionFilter
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Called before the action executes, after model binding is complete.
+        /// </summary>
+        /// <param name="context">The <see cref="ActionExecutingContext"/>.</param>
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            Debug.WriteLine("I am Executing after the action result");
+        }
+                     
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            Debug.WriteLine("I am Executing before the action result");
+        }
+
+    }
+}
